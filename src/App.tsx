a2975c60518beb52554e6d523f9115dc75ad769c
dogmf <dogmf@mail.ru>
2021-10-31 14:16:36 +0300

    helmet and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Menu from './Components/Menu'
 import Post from './pages/Post'
 import About from './pages/About'
 import Main from './pages/Main'
+import { Helmet } from 'react-helmet'
 
 const reactLocation = new ReactLocation()
 
@@ -47,42 +48,50 @@ export const FIRST_LEVEL_ROUTES: MyRoue[] = [
 
 function App() {
   return (
-    <Router
-      location={reactLocation}
-      routes={[
-        {
-          path: '',
-          element: <Main />
-        },
-        ...FIRST_LEVEL_ROUTES,
-        {
-          element: '404'
-        }
-      ]}
-    >
-      <div className="layout spread">
-        <Menu>
-          <Link to="/">Test-app</Link>
-          <ul className="navbar">
-            {FIRST_LEVEL_ROUTES.map(({ path, name }) => {
-              return (
-                <li key={path}>
-                  <Link
-                    to={path}
-                    getActiveProps={() => ({
-                      className: 'active'
-                    })}
-                  >
-                    {name}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </Menu>
-        <Outlet />
-      </div>
-    </Router>
+    <>
+      <Helmet>
+        <title>Test app</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Test app" />
+        <meta property="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <Router
+        location={reactLocation}
+        routes={[
+          {
+            path: '',
+            element: <Main />
+          },
+          ...FIRST_LEVEL_ROUTES,
+          {
+            element: '404'
+          }
+        ]}
+      >
+        <div className="layout spread">
+          <Menu>
+            <Link to="/">Test-app</Link>
+            <ul className="navbar">
+              {FIRST_LEVEL_ROUTES.map(({ path, name }) => {
+                return (
+                  <li key={path}>
+                    <Link
+                      to={path}
+                      getActiveProps={() => ({
+                        className: 'active'
+                      })}
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </Menu>
+          <Outlet />
+        </div>
+      </Router>
+    </>
   )
 }
 
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,8 @@
 import { FC, useMemo } from 'react'
 import { Link, Navigate, useMatch } from 'react-location'
+import { Helmet } from 'react-helmet'
 import POSTS from '../data/posts.json'
+import { truncate } from 'lodash'
 
 const usePost = (postId: string) => {
   return useMemo(() => POSTS.find((p) => p.id === postId), [postId])
@@ -14,6 +16,17 @@ const Post: FC = () => {
   if (!post) return <Navigate to=".." />
   return (
     <div className="content">
+      <Helmet>
+        <title>Post: {post.name}</title>
+        <meta name="description" content={`Post page: ${post.name}`} />
+        <meta
+          property="og:description"
+          content={truncate(post.text, { length: 128 })}
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={post.name} />
+        <meta property="twitter:card" content="summary_large_image" />
+      </Helmet>
       <Link to="..">←</Link>
       <h2>{post.name}</h2>
       <p>{post.text}</p>
